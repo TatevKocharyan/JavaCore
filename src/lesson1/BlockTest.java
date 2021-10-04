5f85dace87ddb06ea802a4c5c76b2d7f6e68d867
TatevKocharyan <tatevik0302@mail.ru>
2021-10-04 15:29:36 +0300

    add new class BlockTest

--- src/lesson1/BlockTest.java
+++ src/lesson1/BlockTest.java
@@ -2,6 +2,16 @@ package lesson1;
 
 public class BlockTest {
     public static void main(String[] args) {
-        int x,y;
+        int x, y;
+        y=20;
+        for (x = 0; x < 10; x++) {
+            System.out.println("This is x:" + x);
+            System.out.println("This is y:" + y);
+            y = y - 2;
+
+
+
+            }
+
+        }
     }
-}
---
+++ src/lesson1/BlockTestExample2.java
@@ -0,0 +1,17 @@
+package lesson1;
+
+public class BlockTestExample2 {
+    public static void main(String[] args) {
+        int i, a;
+        a = 15;
+
+        for (i = 0; i < 20; i++) {
+
+            System.out.println("This is variable i  " + i);
+            System.out.println("This is variable a  " + a);
+            a = a - 5;
+        }
+    }
+}
+
+
