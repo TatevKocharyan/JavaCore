2abb42118497de6929751074f80b8767aa8e8344
TatevKocharyan <tatevik0302@mail.ru>
2021-10-08 18:46:04 +0400

    samples of the book

---
+++ src/chapter3/Array.java
@@ -0,0 +1,20 @@
+package chapter3;
+
+public class Array {
+    public static void main(String[] args) {
+        int [] daysofmonths; daysofmonths= new int[13];
+        daysofmonths[1]=31;
+        daysofmonths[2]=28;
+        daysofmonths[3]=31;
+        daysofmonths[4]=30;
+        daysofmonths[5]=31;
+        daysofmonths[6]=30;
+        daysofmonths[7]=31;
+        daysofmonths[8]=30;
+        daysofmonths[9]=31;
+        daysofmonths[10]=30;
+        daysofmonths[11]=31;
+        daysofmonths[12]=30;
+        System.out.println("April has"+daysofmonths[4]+ "days");
+    }
+}
