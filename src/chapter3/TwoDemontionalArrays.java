1df89704ca8d696c603c63e792a183bb61ba0c72
TatevKocharyan <tatevik0302@mail.ru>
2021-10-10 16:16:13 +0400

    Figure Painter is Done

--- src/chapter3/TwoDemontionalArrays.java
+++ src/chapter3/TwoDemontionalArrays.java
@@ -4,8 +4,8 @@ public class TwoDemontionalArrays {
     public static void main(String[] args) {
         int twoD[][] = new int[4][5];
         int i, j, k = 0;
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 5; j++) {
+        for ( i = 0; i < 4; i++)
+            for ( j = 0; j < 5; j++) {
                 twoD[i][j] = k;
                 k++;}
         for (i = 0; i < 4; i++) {
---
+++ src/homework/FigurePainter.java
@@ -0,0 +1,92 @@
+package homework;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+
+
+
+        // First
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" * ");
+
+            }
+            System.out.println();
+
+        }
+
+
+
+
+
+        // Second
+        for (int i = 4; i > 0; i--) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 5; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        //Third
+        int i;
+        for (i = 0; i < 5; i++) {
+            System.out.println();
+            for (int j = 4; j > i; j--) {
+                System.out.print(" * ");
+
+            }
+        }
+
+        //Fourth
+        int i1;
+        for (i1 = 0; i1 < 5; i1++) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 4; j > i1; j--) {
+                System.out.print("* ");
+            }System.out.println();
+        }
+
+        //Fifth
+        for (int i2 =0; i2 <=3; i2++) {
+            for (int j =3; j >=i2; j--) {
+                System.out.print(" ");
+
+            }
+            for (int j =0; j <= i2; j++) {
+                System.out.print("* ");
+
+            } System.out.println();
+
+        }
+        for (int i2 = 0; i2<=4; i2++) {
+            for (int j = 0; j < i2; j++) {
+                System.out.print(" ");
+            }System.out.print(" ");
+
+            for (int j = 3; j > i2; j--) {
+                System.out.print(" *");
+            }System.out.println();
+        }
+
+
+    }
+
+
+        }
+
+
+
+
+
+
+
+
+
