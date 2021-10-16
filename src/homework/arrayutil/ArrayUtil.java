3d5147536ed1443561187cab1fb7b18bca5c8b3e
TatevKocharyan <tatevik0302@mail.ru>
2021-10-17 02:28:06 +0400

    ArrayUtil+

--- src/homework/arrayutil/ArrayUtil.java
+++ src/homework/arrayutil/ArrayUtil.java
@@ -6,11 +6,12 @@ public class ArrayUtil {
 
         // Task 1Տպեք մասիվի բոլոր էլեմենտները,
         System.out.print("Մասիվի բոլոր էլեմենտներն են`");
-
         for (int i = 0; i < 10; i++) {
             System.out.print((array[i]) + " ");
         }
         System.out.println();
+
+
 
 
         //Task 2Տպեք մասիվի ամենամեծ թիվը,
@@ -19,14 +20,21 @@ public class ArrayUtil {
             if (array[i] > max)
                 max = array[i];
 
-        }System.out.println("Մասիվի ամենամեծ թիվը `"+max);
+        }
+        System.out.println("Մասիվի ամենամեծ թիվը `" + max);
+
+
 
         //Task 3Տպեք մասիվի ամենափոքրը թիվը,
         int min = array[0];
         for (int i = 0; i < array.length; i++) {
             if (array[i] < min)
-                min = array[i];}
-        System.out.println("Մասիվի ամենափոքրը թիվը` " +min);
+                min = array[i];
+        }
+        System.out.println("Մասիվի ամենափոքրը թիվը` " + min);
+
+
+
 
         //Task 4Տպեք մասիվի բոլոր զույգ էլեմենտները,
         System.out.print("Մասիվի զույգ էլեմենտներն են`");
@@ -38,29 +46,56 @@ public class ArrayUtil {
         //Task 5 Տպեք մասիվի բոլոր կենտ  էլեմենտները։
         System.out.print("Մասիվի կենտ էլեմենտներն են`");
         for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 1)
+            if (array[i] % 2 != 1)
                 System.out.print(array[i] + " ");
 
         }
-
-        //Task 6 Տպեք զույգերի քանակը։
-
+        System.out.println();
 
 
 
-        //Task 7 Տպեք կենտերի քանակը
+        //Task 5 Տպեք զույգերի քանակը։
+        int countofEvens = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                countofEvens++;
+            }
+        }
+        System.out.print("Մասիվի զույգ էլեմենտների քանակն է`" + countofEvens);
+
+
+        //Task 6 Տպեք կենտերի քանակը։
+        int countOfodds = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 1) {
+                countOfodds++;
+            }
+        }
+        System.out.println(" Մասիվի կենտ էլեմենտների քանակն է " + countOfodds);
+
+
+
+        //Task 7 Տպեք մասիվի էլեմենտների գումարը։
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > sum) ;
+            sum += array[i];
+        }
+        System.out.println("Մասիվի էլեմենտների գումարն է " + sum);
+
 
 
         //Task 8 Տպեք մասիվի բոլոր թվերի միջին թվաբանականը
-        System.out.println();
-        double result = array[0] + array[1] + array[2] + array[3] + array[4] + array[5] + array[6] + array[7] + array[8] + array[9];
-        System.out.println("Մասիվի թվերի միջին թվաբանականն է " + result / array.length);
-        //Տպեք մասիվի էլեմենտների գումարը։
+        int sum1 = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > sum1) ;
+            {
+                sum1 += array[i];
+            }
+        }
+        double average = sum1;
 
-
-
-
-
+        System.out.println("Մասիվի թվերի միջին թվաբանականն է " + average / array.length);
 
 
     }
