2a7d1d14979241aa9cf38b8c6be6bd0d133c650d
TatevKocharyan <tatevik0302@mail.ru>
2021-10-17 22:53:14 +0400

    Bolola

---
+++ src/homework/arrayutil/Bolola.java
@@ -0,0 +1,22 @@
+package homework.arrayutil;
+
+public class Bolola {
+    public static void main(String[] args) {
+        char[] bolola = {'b', 'o', 'l', 'o', 'l', 'a'};
+
+        int count = 0;
+        for (int i = 0; i < bolola.length; i++) {
+            char c = bolola[i];
+            if (c == 'o') ;
+            {
+                count++;
+            }
+
+
+        }
+        System.out.println("count = " + count);
+
+
+    }
+}
+
